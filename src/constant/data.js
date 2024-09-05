d0958208e7bc220419c929c2a2a23b0f858f80ca
Dilip singh <dilipsinghf@gmail.com>
2024-09-05 10:03:00 +0530

    add routing file .htaccess file in public html file

--- src/constant/data.js
+++ src/constant/data.js
@@ -3,5 +3,5 @@ export const navItems = [
     { id: 'about', label: 'About', href: '/#about' },
     { id: 'services', label: 'Services', href: '/#services' },
     // { id: 'testimonials', label: 'Testimonials', href: '#testimonials' },
-    { id: 'contact', label: 'Contact', href: '/contactus' }
+    { id: 'contact', label: 'Contact', href: './contactus' }
 ];
